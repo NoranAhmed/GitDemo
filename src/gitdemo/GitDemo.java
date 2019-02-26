d38f209b38389e3b4817992835cbb632aea10f86
Noran N. Ahmed <Noran N. Ahmed@DESKTOP-R7E5E9G>
2019-02-26 22:27:32 +0200

    starting the code

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author Noran N. Ahmed
+ */
+public class GitDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "Noran";
+        String city = "Cairo"; 
+        String DOB = "01/01/1997";
+        String college = "Faculty of Engineering";
+        
+        System.out.println("My name is "+ name);
+        System.out.println("Was born in "+city +"on"+DOB);
+        
+    }
+    
+}
